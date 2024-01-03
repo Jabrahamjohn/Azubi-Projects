0724348e3abfb5217c55a025e2d205d1307d9d51
Abraham John <101580587+Jabrahamjohn@users.noreply.github.com>
2024-01-03 13:59:32 +0300

    tip calculator
    The program asks the user to enter the charge for the food.
    
    It then calculates the amounts of an 18 percent tip and 7 percent sales tax on the charge of the food and display each of these amounts.
    
    Finally, it adds everything together and display the charge of the food plus tip and sales tax.
    
    Based on this data, your program should generate script that meets the requirements.

---
+++ tip calculator.py
@@ -0,0 +1,9 @@
+# tip calculator for hotel Asanka
+Charge_of_food = float(input("please input the amount charged for food\n :$"))
+Tip = Charge_of_food * (18/100)
+Sales_tax = Charge_of_food * (7/100)
+Grand_total = Charge_of_food + Tip + Sales_tax
+print("Your bill is as follows:\n")
+print("Tip = ",Tip)
+print(f"\nSales tax = ${Sales_tax: .2f}")
+print("\nYour Grand Total is:\n", Grand_total)
