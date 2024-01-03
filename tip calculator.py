84f350cb470c82f3c03bcc973f5177ad13ab2807
Abraham John <101580587+Jabrahamjohn@users.noreply.github.com>
2024-01-03 14:19:47 +0300

    Update tip calculator.py into a script

--- tip calculator.py
+++ tip calculator.py
@@ -1,4 +1,5 @@
 # tip calculator for hotel Asanka
+#!/usr/bin/python3
 Charge_of_food = float(input("please input the amount charged for food\n :$"))
 Tip = Charge_of_food * (18/100)
 Sales_tax = Charge_of_food * (7/100)
